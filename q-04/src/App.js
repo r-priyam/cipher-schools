76f218a00b51178485f1984c280d0c671b6dfade
Priyam <50884372+r-priyam@users.noreply.github.com>
2022-06-14 20:03:15 +0530

    chore: question four

---
+++ q-04/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <span className="cipher">
+      <h1>Something</h1>
+      <h1>Good</h1>
+      <h1>Here</h1>
+    </span>
+  );
+}
+
+export default App;
