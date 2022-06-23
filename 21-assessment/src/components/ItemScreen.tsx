ff9e73d263ac6e082cd90fbc23efd38a0eb85ef0
Priyam <50884372+r-priyam@users.noreply.github.com>
2022-06-23 14:33:20 +0530

    chore: initial setup

--- 21-assessment/src/components/ItemScreen.tsx
+++ 21-assessment/src/components/ItemScreen.tsx
@@ -1,36 +1,50 @@
-import { useState } from 'react'
+import { Component } from 'react'
 import Item from './Item'
 
-const InitialData = [
-    { id: 1, name: 'Item One', price: 10, count: 0 },
-    { id: 2, name: 'Item Two', price: 20, count: 0 },
-    { id: 3, name: 'Item Three', price: 30, count: 0 },
-    { id: 4, name: 'Item Four', price: 40, count: 0 },
-    { id: 5, name: 'Item Five', price: 50, count: 0 },
-]
+export default class ItemScreen extends Component {
+    initialData = [
+        { id: 1, name: 'Item One', price: 10, count: 0 },
+        { id: 2, name: 'Item Two', price: 20, count: 0 },
+        { id: 3, name: 'Item Three', price: 30, count: 0 },
+        { id: 4, name: 'Item Four', price: 40, count: 0 },
+        { id: 5, name: 'Item Five', price: 50, count: 0 },
+    ]
+    map: Map<number, any> = new Map()
 
-export default function ItemScreen() {
-    let map: Map<number, any> = new Map()
-    for (let item of InitialData) {
-        map.set(item.id, item)
+    constructor(props: any) {
+        super(props)
+        for (let item of this.initialData) {
+            this.map.set(item.id, item)
+        }
+        state = {
+            items: this.map,
+        }
     }
-    const [items, setItems] = useState(map)
 
-    function handleCountClick(id: number, count: number) {
+    handleCountClick(id: number, count: number) {
         if (count < 0) {
             alert('Not Allowed')
-            return;
+            return
         }
-        let testMap = new Map(items)
+        let testMap = new Map(this.state.items)
         let item = testMap.get(id)
         item.count = count
-        setItems(testMap)
+        this.setState({ ...this.state, items: testMap })
     }
-    return (
-        <div>
-            {[...items.values()].map((currentItem, index) => {
-                return <Item key={index} item={currentItem} handleCountClick={handleCountClick} />
-            })}
-        </div>
-    )
+
+    render() {
+        return (
+            <div>
+                {[...this.state.items.values()].map((currentItem, index) => {
+                    return (
+                        <Item
+                            key={index}
+                            item={currentItem}
+                            handleCountClick={this.handleCountClick}
+                        />
+                    )
+                })}
+            </div>
+        )
+    }
 }
---
+++ portfolio/tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ['./src/**/*.{ts,tsx,mdx,html}'],
+    darkMode: 'class',
+    theme: {
+        extend: {}
+    },
+    plugins: []
+};
