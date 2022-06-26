546012a22ac9d6869d919abf638aa968e2073d9d
Priyam <50884372+r-priyam@users.noreply.github.com>
2022-06-26 18:55:06 +0530

    feat: tailwind css

--- test/src/components/Item.jsx
+++ test/src/components/Item.jsx
@@ -1,48 +1,25 @@
-import './Item.css'
 import DemoImage from './image.png'
 
 export default function Item(props) {
     const { data } = props
     return (
-        <div className="itembox">
+        <div className="w-full h-42 border m-4 border-black grid grid-cols-4">
             <div>
-                <img src={DemoImage} height={100}></img>
-                <span
-                    style={{
-                        float: 'left',
-                        marginLeft: '10px',
-                        position: 'absolute',
-                        marginTop: '18px',
-                    }}
-                >
-                    {data.name}
-                </span>
-            </div>
-            <div style={{ display: 'block', position: 'absolute' }}>
-                <span
-                    style={{
-                        float: 'left',
-                        marginLeft: '400px',
-                    }}
-                >
-                    {data.company}
-                </span>
-                <p
-                    style={{
-                        float: 'left',
-                        marginLeft: '400px',
-                    }}
-                >
-                    {data.companyAddress}
-                </p>
+                <img src={DemoImage} className="h-20"></img>
+                <span className="absolute ml-24 -mt-16">{data.name}</span>
+                <span className="absolute ml-24 -mt-8">{data.tags}</span>
             </div>
             <div>
-                <span>{data.price}</span>
-                <p>{data.shipping}</p>
+                <span className="relative -mt-16">{data.company}</span>
+                <p>{data.companyAddress}</p>
             </div>
             <div>
-                <button style={{ display: 'block', float: 'right', marginBottom: '10px' }}>Add To Cart</button>
-                <button style={{ display: 'block', float: 'right' }}>Make Offer</button>
+                <span>${data.price}Aud</span>
+                <p className="ml-4">+ ${data.shipping} Shipping</p>
+            </div>
+            <div className="absolute top-auto right-2 ml-auto inline-flex flex-col items-end justify-end">
+                <button>Add To Cart</button>
+                <button>Make Offer</button>
             </div>
         </div>
     )
---
+++ test/tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ['./src/**/*.{js,jsx,mdx,html}'],
+    darkMode: 'class',
+    theme: {
+        extend: {}
+    },
+    plugins: []
+};
